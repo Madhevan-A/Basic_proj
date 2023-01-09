b06108681f02e97fb5543a537a526078d088f5f6
Madhevan <madhevan.ap@gmail.com>
2023-01-09 10:04:30 +0530

    Commit log check

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ if __name__ == '__main__':
     print("new print")
     print("After Commit")
     print("Log check")
+    print("Push directly")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
