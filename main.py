ccdf303ac391327e9bf894ae57a6395a7ab185bb
Madhevan <madhevan.ap@gmail.com>
2023-01-09 09:49:37 +0530

    Commit 2

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("new print")
+    print("After Commit")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
